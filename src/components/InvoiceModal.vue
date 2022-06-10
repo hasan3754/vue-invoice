f2fa75aa8c6884006e5f4724ea42a99ca7182996
mirazhasan47 <mirazlaradev@gmail.com>
2022-06-10 16:40:45 +0600

    modal transition

--- src/components/InvoiceModal.vue
+++ src/components/InvoiceModal.vue
@@ -78,6 +78,38 @@
                 <label for="productDescription">Product Description</label>
                 <input required type="text" id="productDescription" v-model="productDescription" />
             </div>
+            <div class="work-items">
+                <h3>item List</h3>
+                <table class="item-list">
+                    <tr class="table-heading flex">
+                        <th class="item-name">Item Name</th>
+                        <th class="qty">Qty</th>
+                        <th class="price">Price</th>
+                        <th class="total">Total</th>
+                    </tr>
+                    <tr class="table-items flex" v-for="(item, index) in invoiceItemList" :key="index">
+                        <th class="item-name"><input type="text" v-model="item.itemName" /></th>
+                        <th class="price"><input type="text" v-model="item.price" /></th>
+                        <th class="qty"><input type="text" v-model="item.qty" /></th>
+                        <th class="total flex">{{ (item.total = item.qty * item.price) }}</th>
+                        <th class="qty"><input type="text" v-model="item.itemName" /></th>
+                        <img @click="deleteInvoiceItem(item.id)" src="@/assets/icon-delete.svg" alt=""  />
+                    </tr>
+                </table>
+                <div @click="addNewInvoiceItem" class="flex button">
+                    <img src="@/assets/icon-plus.svg" alt="">
+                    Add New Item
+                </div>
+            </div>
+        </div>
+        <div class="save flex">>
+            <div class="left">
+                <button @click="closeInvoice" class="red">Cancel</button>
+            </div>
+            <div class="right flex">
+                <button @click="saveDraft" class="dark-purple">Save Draft</button>
+                <button @click="publishInvoice" class="purple">Create Invoice</button>                
+            </div>
         </div>
     </form>
    </div>
@@ -115,6 +147,141 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.invoice-wrap {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  @media (min-width: 900px) {
+    left: 90px;
+  }
+  .invoice-content {
+    position: relative;
+    padding: 56px;
+    max-width: 700px;
+    width: 100%;
+    background-color: #141625;
+    color: #fff;
+    box-shadow: 10px 4px 6px -1px rgba(0, 0, 0, 0.2), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+    h1 {
+      margin-bottom: 48px;
+      color: #fff;
+    }
+    h3 {
+      margin-bottom: 16px;
+      font-size: 18px;
+      color: #777f98;
+    }
+    h4 {
+      color: #7c5dfa;
+      font-size: 12px;
+      margin-bottom: 24px;
+    }
+    // Bill To / Bill From
+    .bill-to,
+    .bill-from {
+      margin-bottom: 48px;
+      .location-details {
+        gap: 16px;
+        div {
+          flex: 1;
+        }
+      }
+    }
+    // Invoice Work
+    .invoice-work {
+      .payment {
+        gap: 24px;
+        div {
+          flex: 1;
+        }
+      }
+      .work-items {
+        .item-list {
+          width: 100%;
+          // Item Table Styling
+          .table-heading,
+          .table-items {
+            gap: 16px;
+            font-size: 12px;
+            .item-name {
+              flex-basis: 50%;
+            }
+            .qty {
+              flex-basis: 10%;
+            }
+            .price {
+              flex-basis: 20%;
+            }
+            .total {
+              flex-basis: 20%;
+              align-self: center;
+            }
+          }
+          .table-heading {
+            margin-bottom: 16px;
+            th {
+              text-align: left;
+            }
+          }
+          .table-items {
+            position: relative;
+            margin-bottom: 24px;
+            img {
+              position: absolute;
+              top: 15px;
+              right: 0;
+              width: 12px;
+              height: 16px;
+            }
+          }
+        }
+        .button {
+          color: #fff;
+          background-color: #252945;
+          align-items: center;
+          justify-content: center;
+          width: 100%;
+          img {
+            margin-right: 4px;
+          }
+        }
+      }
+    }
+    .save {
+      margin-top: 60px;
+      div {
+        flex: 1;
+      }
+      .right {
+        justify-content: flex-end;
+      }
+    }
+  }
+  .input {
+    margin-bottom: 24px;
+  }
+  label {
+    font-size: 12px;
+    margin-bottom: 6px;
+  }
+  input,
+  select {
+    width: 100%;
+    background-color: #1e2139;
+    color: #fff;
+    border-radius: 4px;
+    padding: 12px 4px;
+    border: none;
+    &:focus {
+      outline: none;
+    }
+  }
+}
 </style>
